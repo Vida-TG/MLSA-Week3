c64afa1e9afcb3cdbcb9f67e9cba37805969454e
Vida-TG <victorademiju.t@gmail.com>
2023-09-04 18:40:40 +0100

    Mon Sept 4

--- index.js
+++ index.js
@@ -1,27 +1,55 @@
-const fs = require("fs")
+const express = require("express");
+//const http = require("http")
+const app = express()
 
-fs.writeFile("unknown.txt", "I dont see anything", (err) => {
-    if(err) {
-        console.log("Not created");
-    } else {
-        console.log("created")
+const PORT = 5000
+let page = ""
+
+app.get('/', (req, res) => {
+    page = "Homepage2.0"
+    return res.json({page:page})
+
+})
+
+app.post('/', (req, res) => {
+    page = "POST Homepage"
+    return res.json({page})
+})
+
+app.get('/user', (req, res) => {
+    page = "GET a User"
+    return res.json({page:page})
+})
+
+
+app.post('/user', (req, res) => {
+    page = "Users page (POST)"
+    return res.json({page:page})
+})
+
+app.post('/user/:id', (req, res) => {
+    const { id } = req.params
+    page = `Im getting ${id}th user`
+    return res.json({page:page})
+})
+
+
+app.listen(PORT, () => {
+    `Server is live at port ${PORT}`
+})
+
+
+
+/*
+//Routing
+http.createServer((req, res) =>{
+    if(req.url == "/") {
+        res.w
+        if get
+        if po
+    }
+    if(req.url == "/user") {
+        return res.write("Users")
     }
 })
-
-fs.readFile("unknown.txt", "utf-8", (data, err) => {
-    if(err) {
-        console.log(err)
-    } else {
-        console.log(data)
-    }
-})
-
-fs.appendFile("unknown.txt", "\n\nThis was appended", (err) => {
-    if (err) console.log("Could not append")
-    else console.log("Appended")
-})
-
-fs.unlink("unknown.txt", (err) => {
-    if (err) console.log("Could not delete")
-    else console.log("Deleted")
-})
+*/
